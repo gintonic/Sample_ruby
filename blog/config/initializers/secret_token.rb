27551f3b024f19be98ed3a30eba4081eef45ce02
Gintonic <mycefiroa31@hotmail.com>
2013-03-03 01:12:05 +0700

    Add Web Application's name 'blog'
    - Add all Folders and files of Web Application's name "blog"
    - Dev By Ruby on Rail

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '5921d39abeab6799ecad6ea79696d0fa687e1827a83868322afcdf9bc52b8d25a8c755f464d99b989da7398c1b32d6a04958b44026b4b12a9303c02aea75c055'
